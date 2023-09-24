60d5f37cf9a36073df6354344a7fb212cb633c66
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2023-09-24 14:49:27 -0700

    added upper and lower age limits

--- ageLimits.js
+++ ageLimits.js
@@ -7,10 +7,17 @@ function oldestGirl(age) {
     return Math.floor(age + (age - youngestGirl(age)) / 2)
 }
 
-function submitAge(){
+function submitAge() {
     const {value} = document.getElementById("age");
     const header = document.getElementById("result");
-   const youngestAge = youngestGirl(parseInt(value))
-   const oldestAge = oldestGirl(parseInt(value))
+    const youngestAgeAllowed = 14
+    const oldestAgeAllowed = 120
+
+    if (value >= youngestAgeAllowed && value <= oldestAgeAllowed) {
+    const youngestAge = youngestGirl(parseInt(value))
+    const oldestAge = oldestGirl(parseInt(value))
     header.innerHTML = `Youngest age: ${youngestAge} Oldest age: ${oldestAge}`;
+    }else{
+       header.innerHTML = `Please enter an age between ${youngestAgeAllowed} and ${oldestAgeAllowed}`;
+    }
 }
